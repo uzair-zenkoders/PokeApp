eb753dcb98d1182c75e9e17e13613a2481bc4b7c
uzair-zenkoders <uzairzenkoders@gmail.com>
2023-12-21 16:10:00 +0500

    @

--- src/services/firebase-config.ts
+++ src/services/firebase-config.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { FirebaseError, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 // firebase func import(s)
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
